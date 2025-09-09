a911a90b2b0cb5d091b717a702a1c04d1cfc554f
Ricardo Lima Caratti <80797440+desenvolvedorabc@users.noreply.github.com>
2025-09-09 17:52:18 -0300

    Zerando COntexto

--- compile_all.sh
+++
@@ -1,4 +0,0 @@
-arduino-cli compile --fqbn esp8266:esp8266:generic  ./source/esp01/mqtt/dht11/dht11_monitor/dht11_monitor.ino  --output-dir ~/Downloads/hex/esp8266/dht11_monitor  --warnings all --verbose
-arduino-cli compile --fqbn esp8266:esp8266:generic  ./source/esp01/mqtt/motion/motion_sensor.ino  --output-dir ~/Downloads/hex/esp8266/motion_sensor  --warnings all --verbose
-arduino-cli compile --fqbn esp8266:esp8266:generic  ./source/esp01/mqtt/relay/relay.ino  --output-dir ~/Downloads/hex/esp8266/relay  --warnings all --verbose
-arduino-cli compile --fqbn esp8266:esp8266:generic  ./source/esp01/mqtt/RDA5807_MQTT_CONTROL/RDA5807_MQTT_CONTROL.ino  --output-dir ~/Downloads/hex/esp8266/RDA5807_MQTT_CONTROL  --warnings all --verbose
--- raspberry_pi3/start_audio_simulator.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "🎵 Starting HomeGuard Audio Presence Simulator..."
-source homeguard-audio-env/bin/activate
-python audio_presence_simulator.py
